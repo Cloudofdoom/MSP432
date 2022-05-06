75f0de2ea92d5938ada0df1f36347fed3a4d51d6
moostang1 <105025510+moostang1@users.noreply.github.com>
2022-05-05 17:29:23 -0700

    Updated key code

--- Key.c
+++ Key.c
@@ -1,44 +1,89 @@
 #include "msp.h"
+#include <stdint.h>                            // Standard Integer - data type definitions
+#include <stdio.h>                             // Standard I/O definitions
+#include <stdarg.h>
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
-/*
-*
-* This program toggles  LED1 when S1 is pressed
-*
-*/
-void LED1_Initialization(){
-    // initialization - P1_0 for LED1 output
-    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
-    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-}
-void S1_initialization(){
-    // initialization - P1_1 for switch button input
-    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
-}
-void main(void)
+
+inline void uart_println(const char* str, ...)
 {
-    // initializing GPIO (P1.0) for LED1
-    LED1_Initialization();
-    // initializing P1_1 for switch button input
-    S1_initialization();
-    // initializing GPIO Interrupt on P1.1
-    Interrupt_disableMaster();                                                          // 1. start critical section by disabling all interrupts
-    P1interrupt();
-    Interrupt_enableMaster();                                                           // 7. end critical section by enabling all interrupts
-    while(1){
-        __WFI();    // (WatchForInterrupt) put MSP432 in sleep mode to save power until interrupt (S1 closed) kicks in
-    }
-}
-void PORT1_IRQHandler(void)
-{
-    MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
-    GPIO_clearInterruptFlag (GPIO_PORT_P1, GPIO_PIN1);
+  static char print_buffer[256];
+  va_list lst;
+  va_start(lst, str);
+  vsnprintf(print_buffer, 256, str, lst);
+  str = print_buffer;
+  while (*str)
+  {
+    while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
+    UCA0TXBUF = *str;
+    str++;
+  }
+  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
+  UCA0TXBUF = '\r';
+  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
+  UCA0TXBUF = '\n';
 }
 
-void P1interrupt(void)
+const eUSCI_UART_Config uartConfig =
 {
-    GPIO_interruptEdgeSelect(GPIO_PORT_P1, GPIO_PIN1, GPIO_HIGH_TO_LOW_TRANSITION);      // 2. set GPIO interrupt on P1.1 with Falling edge trigger
-    GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN1);                                   // 3. clear GPIO interrupt Flag corresponding to P1.1
-    GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);                                      // 4. enable GPIO interrupt on P1.1
-    Interrupt_setPriority(INT_PORT1, 2);                                                // 5. set priority of interrupt on P1.1
-    Interrupt_enableInterrupt(INT_PORT1);                                               // 6. enable P1 interrupt in NVIC
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        1,                                        // BRDIV = 1
+        10,                                       // UCxBRF = 10
+        0,                                       // UCxBRS = 0
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
+};
+
+void buttons(void){
+     if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) == 0){
+        GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN0);       //turn on red
+        uart_println("Switch 1 Pressed. RGB Set To Red");
+        __delay_cycles(600000);
+    }
+    else if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
+        GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
+        uart_println("Switch 2 Pressed. RGB Set To Green");
+        __delay_cycles(600000);
+    }
+    else{
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);
+    }
+}
+
+void Declare(void){
+    //Set up
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2, PIN_ALL16);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, 0x0007);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN4);
+}
+
+void main(void)
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+
+    /* Halting WDT  */
+        MAP_WDT_A_holdTimer();
+
+        /* Setting DCO to 12MHz */
+           CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3); // to select 3 MHZ
+
+        /* Selecting P1.2 and P1.3 in UART mode */
+        MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+                GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+             /* Configuring UART Module */
+        MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+
+        /* Enable UART module */
+        MAP_UART_enableModule(EUSCI_A0_BASE);
+
+        Declare();
+        while(1){
+            buttons();
+        }
+
 }
