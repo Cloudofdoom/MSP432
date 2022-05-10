94e9a9dc98b9633d9d2fac4d77e0952362c2065d
Cloudofdoom <99325854+Cloudofdoom@users.noreply.github.com>
2022-05-09 20:03:16 -0700

    Added Timer
    Switches don't work when LED() function (TimerA) is running

--- Key.c
+++ Key.c
@@ -1,27 +1,8 @@
 #include "msp.h"
-#include <stdint.h>                            // Standard Integer - data type definitions
-#include <stdio.h>                             // Standard I/O definitions
+#include <stdint.h>
+#include <stdio.h>
 #include <stdarg.h>
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
-
-inline void uart_println(const char* str, ...)
-{
-  static char print_buffer[256];
-  va_list lst;
-  va_start(lst, str);
-  vsnprintf(print_buffer, 256, str, lst);
-  str = print_buffer;
-  while (*str)
-  {
-    while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
-    UCA0TXBUF = *str;
-    str++;
-  }
-  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
-  UCA0TXBUF = '\r';
-  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
-  UCA0TXBUF = '\n';
-}
 
 const eUSCI_UART_Config uartConfig =
 {
@@ -36,15 +17,42 @@ const eUSCI_UART_Config uartConfig =
         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
 };
 
+void UART(void){
+     /* Setting DCO to 12MHz */
+     CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3); // to select 3 MHZ
+
+     /* Selecting P1.2 and P1.3 in UART mode */
+     MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+     /* Configuring UART Module */
+     MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+
+     /* Enable UART module */
+     MAP_UART_enableModule(EUSCI_A0_BASE);
+}
+
+const Timer_A_UpModeConfig upModeConfigP1 = {
+    //CLK Source (SMCLK - 3MHz)
+    TIMER_A_CLOCKSOURCE_SMCLK,
+    //CLK Source Divider = 48 [3MHz/48 = 62500Hz CLK]
+    TIMER_A_CLOCKSOURCE_DIVIDER_48,
+    //Timer Period (Set Max Count Value [62500 = CLK = Interrupt Every Second)
+    62500,
+    //Interrupt Enable
+    TIMER_A_TAIE_INTERRUPT_ENABLE,
+    //Clear
+    TIMER_A_DO_CLEAR
+};
+
 void buttons(void){
      if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) == 0){
         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN0);       //turn on red
-        uart_println("Switch 1 Pressed. RGB Set To Red");
+        UART_transmitData(EUSCI_A0_BASE, 0x72);
         __delay_cycles(600000);
     }
     else if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
-        uart_println("Switch 2 Pressed. RGB Set To Green");
+        UART_transmitData(EUSCI_A0_BASE, 0x47);
         __delay_cycles(600000);
     }
     else{
@@ -55,35 +63,54 @@ void buttons(void){
 
 void Declare(void){
     //Set up
+
     GPIO_setOutputLowOnPin(GPIO_PORT_P2, PIN_ALL16);
     GPIO_setAsOutputPin(GPIO_PORT_P2, 0x0007);
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN4);
 }
 
+void LED1(void);
+
 void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
     /* Halting WDT  */
-        MAP_WDT_A_holdTimer();
+    MAP_WDT_A_holdTimer();
 
-        /* Setting DCO to 12MHz */
-           CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3); // to select 3 MHZ
+    UART();
 
-        /* Selecting P1.2 and P1.3 in UART mode */
-        MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
-                GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+    Declare();
+    LED1();
+    while(1){
 
-             /* Configuring UART Module */
-        MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+        buttons();
 
-        /* Enable UART module */
-        MAP_UART_enableModule(EUSCI_A0_BASE);
-
-        Declare();
-        while(1){
-            buttons();
-        }
+    }
 
 }
+
+void LED1(void){
+        //Setup LED
+        GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+
+        //Setup Timer_A0 (Can Choose Any Timer_A)
+        Timer_A_configureUpMode(TIMER_A0_BASE, &upModeConfigP1);
+        //Put to sleep and configure interrupt
+        Interrupt_enableSleepOnIsrExit();
+        //Enable interrupt
+        Interrupt_enableInterrupt(INT_TA0_N);
+        //Enabling MASTER interrupts
+        Interrupt_enableMaster();
+        //Start TimerA0
+        Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+
+}
+
+void TA0_N_IRQHandler(void){
+    MAP_Timer_A_clearInterruptFlag(TIMER_A0_BASE);
+    MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1,GPIO_PIN0);
+}
+
