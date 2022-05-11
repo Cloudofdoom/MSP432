d81a706367baba650d4bf2d3765d908a3350f6e8
Cloudofdoom <99325854+Cloudofdoom@users.noreply.github.com>
2022-05-11 00:55:57 -0700

    Timer A (two modes) + GPIO Switches work for testing

--- GPIOKey.c
+++ GPIOKey.c
@@ -44,18 +44,34 @@ const Timer_A_UpModeConfig upModeConfigP1 = {
     TIMER_A_DO_CLEAR
 };
 
+const Timer_A_UpModeConfig upModeConfigP11 = {
+    //CLK Source (SMCLK - 3MHz)
+    TIMER_A_CLOCKSOURCE_SMCLK,
+    //CLK Source Divider = 48 [3MHz/48 = 62500Hz CLK]
+    TIMER_A_CLOCKSOURCE_DIVIDER_48,
+    //Timer Period (Set Max Count Value [62500 = CLK = Interrupt Every Second)
+    7812,
+    //Interrupt Enable
+    TIMER_A_TAIE_INTERRUPT_ENABLE,
+    //Clear
+    TIMER_A_DO_CLEAR
+};
+
 void buttons(void){
      if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) == 0){
         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN0);       //turn on red
         UART_transmitData(EUSCI_A0_BASE, 0x72);
-        LED1();
-        //LED2();
         __delay_cycles(600000);
+        //AlarmEnable();
+        AlarmTriggered();
     }
     else if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
+        Interrupt_disableInterrupt(INT_TA0_N);
         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
         UART_transmitData(EUSCI_A0_BASE, 0x47);
         __delay_cycles(600000);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
     }
     else{
         GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
@@ -65,17 +81,16 @@ void buttons(void){
 
 void Declare(void){
     //Set up
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
+
     GPIO_setOutputLowOnPin(GPIO_PORT_P2, PIN_ALL16);
-    GPIO_setAsOutputPin(GPIO_PORT_P2, 0x0007);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN1);
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN4);
-
-    //Setup LED
-    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
 }
-void LED2(void);
-void LED1(void);
+
+void AlarmEnable(void);
+void AlarmTriggered(void);
 
 void main(void)
 {
@@ -87,54 +102,52 @@ void main(void)
     UART();
 
     Declare();
-    //LED1();
-    //buttons();
+
     while(1){
-        //LED1();
+
         buttons();
 
     }
 
 }
 
-void LED2(void){
-    while(1){
-        //UART();
-        GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-        __delay_cycles(600000);
-        if(UART_receiveData(EUSCI_A0_BASE) == 0x47){
-            break;
-        }
+void AlarmEnable(void){
+        //Setup LED
+        GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
         GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-        __delay_cycles(600000);
-        if(UART_receiveData(EUSCI_A0_BASE) == 0x47){
-            break;
-        }
-    }
+        //Setup Timer_A0 (Can Choose Any Timer_A)
+        Timer_A_configureUpMode(TIMER_A0_BASE, &upModeConfigP1);
+        //Put to sleep and configure interrupt
+        //Interrupt_enableSleepOnIsrExit();
+        Interrupt_disableSleepOnIsrExit();
+        //Enable interrupt
+        Interrupt_enableInterrupt(INT_TA0_N);
+        //Enabling MASTER interrupts
+        Interrupt_enableMaster();
+        //Start TimerA0
+        Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
 }
 
-void LED1(void){
-    __delay_cycles(600000);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
-    while(1){
-        GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-        //__delay_cycles(600000);
-        if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
-            //GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-            GPIO_toggleOutputOnPin(GPIO_PORT_P2, GPIO_PIN1);
-            GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-            //__delay_cycles(600000);
-            break;
-        }
-        __delay_cycles(600000);
+void AlarmTriggered(void){
+        //Setup LED
+        GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
         GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-        //__delay_cycles(600000);
-        if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
-            GPIO_toggleOutputOnPin(GPIO_PORT_P2, GPIO_PIN1);
-            //__delay_cycles(600000);
-            break;
-        }
-        __delay_cycles(600000);
-    }
+        //Setup Timer_A0 (Can Choose Any Timer_A)
+        Timer_A_configureUpMode(TIMER_A0_BASE, &upModeConfigP11);
+        //Put to sleep and configure interrupt
+        //Interrupt_enableSleepOnIsrExit();
+        Interrupt_disableSleepOnIsrExit();
+        //Enable interrupt
+        Interrupt_enableInterrupt(INT_TA0_N);
+        //Enabling MASTER interrupts
+        Interrupt_enableMaster();
+        //Start TimerA0
+        Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
 }
+
+void TA0_N_IRQHandler(void){
+    MAP_Timer_A_clearInterruptFlag(TIMER_A0_BASE);
+    MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1,GPIO_PIN0);
+}
+
 
