2a5db16667be95978ea7078ea9f45e3a434312e3
Cloudofdoom <99325854+Cloudofdoom@users.noreply.github.com>
2022-05-12 17:02:00 -0700

    Update Key.c

--- Key.c
+++ Key.c
@@ -1,27 +1,8 @@
 #include "msp.h"
-#include <stdint.h>                            // Standard Integer - data type definitions
-#include <stdio.h>                             // Standard I/O definitions
+#include <stdint.h>
+#include <stdio.h>
 #include <stdarg.h>
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
-
-inline void uart_println(const char* str, ...)
-{
-  static char print_buffer[256];
-  va_list lst;
-  va_start(lst, str);
-  vsnprintf(print_buffer, 256, str, lst);
-  str = print_buffer;
-  while (*str)
-  {
-    while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
-    UCA0TXBUF = *str;
-    str++;
-  }
-  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
-  UCA0TXBUF = '\r';
-  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
-  UCA0TXBUF = '\n';
-}
 
 const eUSCI_UART_Config uartConfig =
 {
@@ -36,16 +17,28 @@ const eUSCI_UART_Config uartConfig =
         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
 };
 
+void UART(void){
+     /* Setting DCO to 12MHz */
+     CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3); // to select 3 MHZ
+
+     /* Selecting P1.2 and P1.3 in UART mode */
+     MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+     /* Configuring UART Module */
+     MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+
+     /* Enable UART module */
+     MAP_UART_enableModule(EUSCI_A0_BASE);
+}
+
 void buttons(void){
      if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) == 0){
         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN0);       //turn on red
-        uart_println("Switch 1 Pressed. RGB Set To Red");
-        __delay_cycles(600000);
+        UART_transmitData(EUSCI_A0_BASE, 0x72);
     }
-    else if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
-        GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
-        uart_println("Switch 2 Pressed. RGB Set To Green");
-        __delay_cycles(600000);
+     else if (GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
+         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
+         UART_transmitData(EUSCI_A0_BASE, 0x47);
     }
     else{
         GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
@@ -55,8 +48,10 @@ void buttons(void){
 
 void Declare(void){
     //Set up
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
+
     GPIO_setOutputLowOnPin(GPIO_PORT_P2, PIN_ALL16);
-    GPIO_setAsOutputPin(GPIO_PORT_P2, 0x0007);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN1);
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
     GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN4);
 }
@@ -66,24 +61,17 @@ void main(void)
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
     /* Halting WDT  */
-        MAP_WDT_A_holdTimer();
+    MAP_WDT_A_holdTimer();
 
-        /* Setting DCO to 12MHz */
-           CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3); // to select 3 MHZ
+    UART();
 
-        /* Selecting P1.2 and P1.3 in UART mode */
-        MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
-                GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+    Declare();
 
-             /* Configuring UART Module */
-        MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+    while(1){
 
-        /* Enable UART module */
-        MAP_UART_enableModule(EUSCI_A0_BASE);
+        buttons();
 
-        Declare();
-        while(1){
-            buttons();
-        }
+    }
 
 }
+
