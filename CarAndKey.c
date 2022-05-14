9fa556f1c69fe62556c332004e11c59cc3d66752
Cloudofdoom <99325854+Cloudofdoom@users.noreply.github.com>
2022-05-14 16:30:33 -0700

    Update CarAndKey.c

--- CarAndKey.c
+++ CarAndKey.c
@@ -1,94 +1,182 @@
-#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include "msp.h"
 #include <stdint.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-//![Simple Timer_A Config]
-/* Timer_A PWM Configuration Parameter */
-/*Timer_A_PWMConfig pwmConfig =
+const eUSCI_UART_Config uartConfig =
 {
-        TIMER_A_CLOCKSOURCE_SMCLK,
-        TIMER_A_CLOCKSOURCE_DIVIDER_1,
-        32000,
-        TIMER_A_CAPTURECOMPARE_REGISTER_1,
-        TIMER_A_OUTPUTMODE_RESET_SET,
-        3200
-};*/
-//![Simple Timer_A Config]
-
-const Timer_A_ContinuousModeConfig continuousModeConfig =
-{
-     TIMER_A_CLOCKSOURCE_ACLK,
-     TIMER_A_CLOCKSOURCE_DIVIDER_1,
-     TIMER_A_TAIE_INTERRUPT_ENABLE,
-     TIMER_A_DO_CLEAR
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        1,                                        // BRDIV = 1
+        10,                                       // UCxBRF = 10
+        0,                                       // UCxBRS = 0
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
 };
 
-void setup_timer(int duty);
+void UART(void){
+     /* Setting DCO to 12MHz */
+     CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3); // to select 3 MHZ
 
-void setup_timer(int duty){
-    // P6.6 in PSEL=01 mode and set as output
-    P6->SEL0 |= 0x40;
-    P6->SEL1 &= ~0x40;
-    P6->DIR |= 0x40;
+     /* Selecting P1.2 and P1.3 in UART mode */
+     MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
-    TIMER_A2->CTL = 0x0210;
-    TIMER_A2->CCR[0] = 3000;
-    TIMER_A2->CCR[3] = 3000*(duty)/100;
-    TIMER_A2->CCTL[3] = 0x00E0;
+     /* Configuring UART Module */
+     MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
 
-    return;
- }
+     /* Enable UART module */
+     MAP_UART_enableModule(EUSCI_A0_BASE);
+}
 
-int main(void)
+const Timer_A_UpModeConfig upModeConfigP1 = {
+    //CLK Source (SMCLK - 3MHz)
+    TIMER_A_CLOCKSOURCE_SMCLK,
+    //CLK Source Divider = 48 [3MHz/48 = 62500Hz CLK]
+    TIMER_A_CLOCKSOURCE_DIVIDER_48,
+    //Timer Period (Set Max Count Value [62500 = CLK = Interrupt Every Second)
+    62500,
+    //Interrupt Enable
+    TIMER_A_TAIE_INTERRUPT_ENABLE,
+    //Clear
+    TIMER_A_DO_CLEAR
+};
+
+const Timer_A_UpModeConfig upModeConfigP11 = {
+    //CLK Source (SMCLK - 3MHz)
+    TIMER_A_CLOCKSOURCE_SMCLK,
+    //CLK Source Divider = 48 [3MHz/48 = 62500Hz CLK]
+    TIMER_A_CLOCKSOURCE_DIVIDER_48,
+    //Timer Period (Set Max Count Value [62500 = CLK = Interrupt Every Second)
+    7812,
+    //Interrupt Enable
+    TIMER_A_TAIE_INTERRUPT_ENABLE,
+    //Clear
+    TIMER_A_DO_CLEAR
+};
+void condition(void){
+    if (UART_receiveData(EUSCI_A0_BASE) == 0x47){
+        //Interrupt_disableInterrupt(INT_TA0_N);
+        AlarmEnable();
+    }
+    else if (UART_receiveData(EUSCI_A0_BASE) == 0x72){
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
+        //Interrupt_disableInterrupt(INT_TA0_N);
+        AlarmTriggered();
+    }
+}
+
+void buttons(void){
+     if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) == 0){
+        GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN0);       //turn on red
+        //UART_transmitData(EUSCI_A0_BASE, 0x72);
+        __delay_cycles(600000);
+        //condition();
+        AlarmEnable();
+        //AlarmTriggered();
+    }
+     /*else if (GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
+         //Interrupt_disableInterrupt(INT_TA0_N);
+         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
+         //UART_transmitData(EUSCI_A0_BASE, 0x47);
+         //__delay_cycles(600000);
+         //condition();
+    }*/
+    else if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
+        Interrupt_disableInterrupt(INT_TA0_N);
+        Interrupt_disableInterrupt(INT_TA2_N);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+        GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
+        //UART_transmitData(EUSCI_A0_BASE, 0x47);
+        __delay_cycles(600000);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN6);
+    }
+    else{
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);
+    }
+}
+
+void Declare(void){
+    //Set up
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
+
+
+
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2, PIN_ALL16);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN1);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN4);
+}
+
+void AlarmEnable(void);
+void AlarmTriggered(void);
+
+void main(void)
 {
-    /* Halting the watchdog */
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+
+    /* Halting WDT  */
     MAP_WDT_A_holdTimer();
 
-    setup_timer(60);
+    UART();
 
-    /* Configuring P1.0 as output */
-    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
-    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    Declare();
 
-    //![Simple Timer_A Example]
-    /* Setting MCLK to REFO at 128Khz for LF mode
-     * Setting SMCLK to 64Khz */
-    MAP_CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
-    //MAP_CS_initClockSignal(CS_MCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
-    MAP_CS_initClockSignal(CS_SMCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
-    MAP_PCM_setPowerState(PCM_AM_LF_VCORE0);
+    while(1){
 
-    /* Configuring GPIO2.4 as peripheral output for PWM  and P6.7 for button
-     * interrupt */
-    //MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4, GPIO_PRIMARY_MODULE_FUNCTION);
-
-    /* Configuring Continuous Mode */
-    MAP_Timer_A_configureContinuousMode(TIMER_A0_BASE, &continuousModeConfig);
-
-    /* Enabling interrupts and going to sleep */
-    MAP_Interrupt_enableSleepOnIsrExit();
-    MAP_Interrupt_enableInterrupt(INT_TA0_N);
-
-    /* Enabling MASTER interrupts */
-    MAP_Interrupt_enableMaster();
-
-    /* Starting the Timer_A0 in continuous mode */
-    MAP_Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_CONTINUOUS_MODE);
-
-
-    /* Configuring Timer_A to have a period of approximately 500ms and
-     * an initial duty cycle of 10% of that (3200 ticks)  */
-    //MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
-    //![Simple Timer_A Example]
-
-
-    while (1)
-    {
-        __WFI();
+        buttons();
+        //condition();
     }
+
+}
+
+void AlarmEnable(void){
+        //Interrupt_disableInterrupt(INT_TA0_N);
+        //Setup LED
+        GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+        //Setup Timer_A0 (Can Choose Any Timer_A)
+        Timer_A_configureUpMode(TIMER_A0_BASE, &upModeConfigP1);
+        //Put to sleep and configure interrupt
+        //Interrupt_enableSleepOnIsrExit();
+        Interrupt_disableSleepOnIsrExit();
+        //Enable interrupt
+        Interrupt_enableInterrupt(INT_TA0_N);
+        //Enabling MASTER interrupts
+        Interrupt_enableMaster();
+        //Start TimerA0
+        Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+}
+
+void AlarmTriggered(void){
+        //Interrupt_disableInterrupt(INT_TA0_N);
+        //Setup LED
+        GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN6);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN6);
+        //Setup Timer_A0 (Can Choose Any Timer_A)
+        Timer_A_configureUpMode(TIMER_A2_BASE, &upModeConfigP11);
+        //Put to sleep and configure interrupt
+        //Interrupt_enableSleepOnIsrExit();
+        Interrupt_disableSleepOnIsrExit();
+        //Enable interrupt
+        Interrupt_enableInterrupt(INT_TA2_N);
+        //Enabling MASTER interrupts
+        Interrupt_enableMaster();
+        //Start TimerA0
+        Timer_A_startCounter(TIMER_A2_BASE, TIMER_A_UP_MODE);
 }
 
 void TA0_N_IRQHandler(void){
     MAP_Timer_A_clearInterruptFlag(TIMER_A0_BASE);
-    MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1,GPIO_PIN0);
+
 }
+void TA2_N_IRQHandler(void){
+    MAP_Timer_A_clearInterruptFlag(TIMER_A2_BASE);
+    MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P5,GPIO_PIN6);
+}
+
