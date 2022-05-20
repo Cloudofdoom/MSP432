b5465b39a0b3304e054bc2beec4930c7af362bf5
Cloudofdoom <99325854+Cloudofdoom@users.noreply.github.com>
2022-05-19 17:48:47 -0700

    Removed comments & function

--- Key.c
+++ Key.c
@@ -29,57 +29,27 @@ const Timer_A_UpModeConfig upModeConfigP11 = {
     //Clear
     TIMER_A_DO_CLEAR
 };
-void condition(void){
-    if (UART_receiveData(EUSCI_A0_BASE) == 0x47){
-        //Interrupt_disableInterrupt(INT_TA0_N);
-        AlarmEnable();
-    }
-    else if (UART_receiveData(EUSCI_A0_BASE) == 0x72){
-        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
-        //Interrupt_disableInterrupt(INT_TA0_N);
-        AlarmTriggered();
-    }
-}
 
 void buttons(void){
      if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) == 0){
         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN0);       //turn on red
-        //UART_transmitData(EUSCI_A0_BASE, 0x72);
-        //__delay_cycles(600000);
-        //condition();
-        //GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN7);
         AlarmEnable();
-        //AlarmTriggered();
-
     }
-     /*else if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN7) == 0){
-         //Interrupt_disableInterrupt(INT_TA0_N);
-         //UART_transmitData(EUSCI_A0_BASE, 0x47);
-         AlarmTriggered();
-         //__delay_cycles(600000);
-         //condition();
-    }*/
     else if(GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == 0){
         Interrupt_disableInterrupt(INT_TA0_N);
         Interrupt_disableInterrupt(INT_TA2_N);
         GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
         GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
-        //UART_transmitData(EUSCI_A0_BASE, 0x47);
         __delay_cycles(600000);
         GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);
         GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN6);
         GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN6);
     }
     else if(GPIO_getInputPinValue(GPIO_PORT_P2, GPIO_PIN4) == 1){
-            Interrupt_disableInterrupt(INT_TA0_N);
-            //Interrupt_disableInterrupt(INT_TA2_N);
-            GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-            //GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1);       //turn on green
-            //UART_transmitData(EUSCI_A0_BASE, 0x47);
-            //__delay_cycles(600000);
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);
-            GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN6);
-            //GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN6);
+        Interrupt_disableInterrupt(INT_TA0_N);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);
+        GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN6);
     }
     else{
         GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
@@ -91,13 +61,6 @@ void buttons(void){
 void Declare(void){
     //Set up
     GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
-
-    //GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN7);
-    //GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN7);
-    //GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN7);
-
-    //GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN4);
-    //GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN4);
 
     GPIO_setAsInputPin(GPIO_PORT_P2, GPIO_PIN4);
 
@@ -120,15 +83,11 @@ void main(void)
     Declare();
 
     while(1){
-
         buttons();
-        //condition();
     }
-
 }
 
 void AlarmEnable(void){
-        //Interrupt_disableInterrupt(INT_TA0_N);
         //Setup LED
         GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
         GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
@@ -152,13 +111,9 @@ void AlarmEnable(void){
 }
 
 void AlarmTriggered(void){
-        //Interrupt_disableInterrupt(INT_TA0_N);
         //Setup LED
         GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN6);
         GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN6);
-
-        //GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN7);
-        //GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN7);
 
         //Setup Timer_A0 (Can Choose Any Timer_A)
         Timer_A_configureUpMode(TIMER_A2_BASE, &upModeConfigP11);
@@ -171,7 +126,6 @@ void AlarmTriggered(void){
         Interrupt_enableMaster();
         //Start TimerA0
         Timer_A_startCounter(TIMER_A2_BASE, TIMER_A_UP_MODE);
-        //GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN7);
 }
 
 void TA0_N_IRQHandler(void){
@@ -179,12 +133,9 @@ void TA0_N_IRQHandler(void){
     MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1,GPIO_PIN0);
     MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P5,GPIO_PIN6);
     GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN6);
-    //MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1,GPIO_PIN6);
 }
 void TA2_N_IRQHandler(void){
     MAP_Timer_A_clearInterruptFlag(TIMER_A2_BASE);
     MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P5,GPIO_PIN6);
     MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1,GPIO_PIN7);
 }
-
-
